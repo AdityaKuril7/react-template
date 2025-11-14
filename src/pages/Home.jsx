c677ad1988ff72c65f36fca9e3027e329759f0a5
AdityaKuril7 <adityakuril760@gmail.com>
2025-11-14 19:22:30 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-center mt-5 font-bold text-2xl text-black">
+        Template By Aditya Kuril
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
